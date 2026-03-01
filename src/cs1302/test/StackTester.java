55b3393b4cb8d86178342fe56a195643a5611fcb
Brad Barnes <bjb211@gmail.com>
2026-03-01 16:15:26 -0500

    Starter code for part 4

--- src/cs1302/test/StackTester.java
+++ src/cs1302/test/StackTester.java
@@ -138,6 +138,156 @@ public class StackTester {
         } // if
     } // stackTest08
 
+    /** Test 9. */
+    private void stackTest09() {
+        System.out.print("9. pop() is the last item pushed after 1 push: ");
+
+        Stack stack = this.newStack();
+        stack.push("a");
+
+        if (stack.pop().equals("a")) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        } // if
+    } // stackTest09
+
+    /** Test 10. */
+    private void stackTest10() {
+        System.out.print("10. 1 call to pop() is correct after 2 pushes: ");
+
+        Stack stack = this.newStack();
+        stack.push("a");
+        stack.push("b");
+
+        if (stack.pop().equals("b")) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        } // if
+    } // stackTest10
+
+    /** Test 11. */
+    private void stackTest11() {
+        System.out.print("11. 2 calls to pop() are correct after 2 pushes: ");
+
+        Stack stack = this.newStack();
+        stack.push("a");
+        stack.push("b");
+
+        if (stack.pop().equals("b") && stack.pop().equals("a")) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        } // if
+    } // stackTest11
+
+    /** Test 12. */
+    private void stackTest12() {
+        System.out.print("12. 1 call to pop() is correct after 3 pushes: ");
+
+        Stack stack = this.newStack();
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+
+        if (stack.pop().equals("c")) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        } // if
+    } // stackTest12
+
+    /** Test 13. */
+    private void stackTest13() {
+        System.out.print("13. 2 calls to pop() are correct after 3 pushes: ");
+        Stack stack = this.newStack();
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+
+        if (stack.pop().equals("c") && stack.pop().equals("b")) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        } // if
+    } // stackTest13
+
+    /** Test 14. */
+    private void stackTest14() {
+        System.out.print("14. 3 calls to pop() are correct after 3 pushes: ");
+
+        Stack stack = this.newStack();
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+        boolean firstPop = stack.pop().equals("c");
+        boolean secondPop = stack.pop().equals("b");
+        boolean thirdPop = stack.pop().equals("a");
+
+        if (firstPop && secondPop && thirdPop) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        } // if
+
+    } // stackTest11
+
+    /** Test 15. */
+    private void stackTest15() {
+        System.out.print("15. size() is 3 after 4 pushes and 1 pop(): ");
+
+        Stack stack = this.newStack();
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+        stack.push("d");
+        stack.pop();
+
+        if (stack.size() == 3) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        } // if
+    } // stackTest15
+
+    /** Test 16. */
+    private void stackTest16() {
+        System.out.print("16. size() is 1 after 3 pushes and 2 pop(): ");
+
+        Stack stack = this.newStack();
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+        stack.pop();
+        stack.pop();
+
+        if (stack.size() == 1) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        } // if
+    } // stackTest16
+
+    /** Test 17. */
+    private void stackTest17() {
+        System.out.print("17. size() is 0 after 3 pushes and 3 pop(): ");
+
+        Stack stack = this.newStack();
+        stack.push("a");
+        stack.push("b");
+        stack.push("c");
+        stack.pop();
+        stack.pop();
+        stack.pop();
+
+        if (stack.size() == 0) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        } // if
+    } // stackTest17
+
     /**
      * Run tests for uses cases that each start with a new
      * {@link Stack} object returned by {@link #newStack()}.
@@ -151,6 +301,15 @@ public class StackTester {
         stackTest06();
         stackTest07();
         stackTest08();
+        stackTest09();
+        stackTest10();
+        stackTest11();
+        stackTest12();
+        stackTest13();
+        stackTest14();
+        stackTest15();
+        stackTest16();
+        stackTest17();
     } // test
 
     public static void main(String[] args) {
